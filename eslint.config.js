142a3eb5591f8f1592ea5d77cbc522d1d230f10f
fariqmanaf <fariqmanaf24@gmail.com>
2025-01-04 13:58:42 +0700

    test: eslint and husky precommit

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,29 @@
-import js from "@eslint/js";
 import globals from "globals";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ["dist"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  { ignores: ["tailwind.config.js"] },
   {
-    files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: "latest",
-        ecmaFeatures: { jsx: true },
-        sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
       },
     },
-    settings: { react: { version: "18.3" } },
-    plugins: {
-      react,
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs["jsx-runtime"].rules,
-      ...reactHooks.configs.recommended.rules,
-      "react/jsx-no-target-blank": "off",
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-    },
   },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { settings: { react: { version: "detect" } } },
   {
-    extends: ["prettier"],
+    rules: {
+      "no-console": "warn",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": ["warn", { args: "none" }],
+    },
   },
 ];
